26799796c4a993eef639d0bd43ae44e5fdca3d16
Simon Balleux <simoncestmoi@hotmail.fr>
2022-03-16 12:00:53 +0100

    Instalation Bootstrap global, formulaire OK, requete ajax jQuerry OK, Onglets OK

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-function create() {
-  $.ajax({
-    method: "POST",
-    url: "create_group.php",
-    data: {
-      Id: document.getElementById("ID").value,
-      Nom: document.getElementById("NAME").value,
-      TelClient1: document.getElementById("TEL1").value,
-      TelClient2: document.getElementById("TEL2").value,
-      TelEcoute: document.getElementById("ECOUTE").value,
-      TelGeneral: document.getElementById("GENERAL").value,
-    },
-  }).done(function (msg) {
-    alert("Data Sauvegardé: " + msg);
-  });
-}
+// Ecouter l'evenement submit et declacher une alert formulaire envoyé + la requete AJAX
+document.getElementById("inscription").addEventListener("submit", function (e) {
+  e.preventDefault();
+  var erreur;
+
+  if (erreur) {
+    return false;
+  } else {
+    $.ajax({
+      method: "POST",
+      url: "create_group.php",
+      data: {
+        Id: document.getElementById("ID").value,
+        Nom: document.getElementById("NAME").value,
+        TelClient1: document.getElementById("TEL1").value,
+        TelClient2: document.getElementById("TEL2").value,
+        TelEcoute: document.getElementById("ECOUTE").value,
+        TelGeneral: document.getElementById("GENERAL").value,
+      },
+    }).done(function (msg) {
+      alert("Data Sauvegardé: " + msg);
+    });
+    alert("Formulaire envoyé !");
+  }
+});
